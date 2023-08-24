e5dd9b18933669ace70927a9080879f61b721017
zzanzu <lees3301@gmail.com>
2023-08-24 14:37:39 +0900

    chore: Hilt 기본 설정 적용

---
+++ app/src/main/java/com/zzanzu/itunes_search_example/WatchaApplication.kt
@@ -0,0 +1,9 @@
+package com.zzanzu.itunes_search_example
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WatchaApplication : Application() {
+
+}
