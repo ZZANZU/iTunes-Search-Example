9e28d5b4aebb1aee0753781300c2ff9c47275849
Chanjoo Lee <lees3301@gmail.com>
2023-08-24 14:15:45 +0900

    feat: BottomNavigationView, 검색/즐겨찾기 프래그먼트 추가 (#1)
    * feat: BottomNavigationView 추가
    
    * feat: 프래그먼트 레이아웃 추가
    
    * feat: 프래그먼트 추가(검색, 즐겨찾기)
    
    * chore: compileSdk 변경
    
    * chore: Navigation 라이브러리 추가
    
    * chore: ViewBinding 적용
    
    * feat: Navigation 프래그먼트 전환 로직 구현

--- app/src/main/java/com/zzanzu/itunes_search_example/MainActivity.kt
+++ app/src/main/java/com/zzanzu/itunes_search_example/MainActivity.kt
@@ -2,10 +2,32 @@ package com.zzanzu.itunes_search_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.zzanzu.itunes_search_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        val hostFragment = supportFragmentManager
+            .findFragmentById(R.id.navigation_host_fragment) as NavHostFragment ?: return
+
+        navController = hostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
